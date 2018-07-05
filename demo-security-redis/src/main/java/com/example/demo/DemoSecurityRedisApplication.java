985593eb8107a7b02df48fbe42bd57046719e639
Sangjun, Park <libedi@gmail.com>
2018-07-05 22:41:41 +0900

    test session invalidate

--- demo-security-redis/src/main/java/com/example/demo/DemoSecurityRedisApplication.java
+++ demo-security-redis/src/main/java/com/example/demo/DemoSecurityRedisApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableRedisHttpSession
 @RestController
-public class DemoSecurityRedisApplication {
+public class DemoSecurityRedisApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSecurityRedisApplication.class, args);
@@ -24,7 +29,15 @@ public class DemoSecurityRedisApplication {
 	}
 	
 	@GetMapping("/user")
-	public String getUser() {
-		return "Hello, User!";
+	public String getUser(@AuthenticationPrincipal UserDetails user) {
+		return new StringBuilder("Hello, ").append(user.getUsername()).append("!").toString();
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")).permitAll();
+	}
+	
+	
 }
